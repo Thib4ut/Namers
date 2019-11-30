da1607fbeb653090dc2d47d37f872636e3686e78
Thibault.INC <39965137+Thib4ut@users.noreply.github.com>
2019-11-30 19:14:01 +0100

    Commited at 19:13

--- templates/src/js/banner.js
+++ templates/src/js/banner.js
@@ -1,1 +1,14 @@
 var Banner = document.querySelector('section#banner');
+
+function BannerAnimation (element) {
+  var elem = element;
+  element.style.background = "#19a635";
+  setInterval(() => {
+    element.style.background = "#263b2a";
+    setTimeout(() => {
+        element.style.background = "#19a635";
+    }, 1000)
+  }, 2000)
+}
+
+BannerAnimation(Banner);
